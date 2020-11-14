cb05fb4b8e8143eff0f39c64130afc371d209fe5
Thalles Veloso <thalleslima8@gmail.com>
2020-11-14 12:17:40 -0300

    Primeiro commit; Criando Projeto para App Web ASP.NET CORE MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PsiConsulta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
